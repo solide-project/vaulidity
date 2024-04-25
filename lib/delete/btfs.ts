8937c51bfae3e60c6fb9a15e1cd4f168b953a2d0
unknown <z5208980@ad.unsw.edu.au>
2024-04-25 13:32:15 +1000

    update contract

--- lib/delete/btfs.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-export default async function btfs(cid: string): Promise<any> {
-    'use server';
-    const response = await fetch(`https://gateway.btfs.io/btfs/${cid}`)
-
-    return response.json()
-}
